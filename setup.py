bab8c8c6bdf86cfb26d8ebcfa2de4ae7a6eacce2
proguy914629 <74696067+proguy914629bot@users.noreply.github.com>
2021-10-18 21:22:59 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ setuptools.setup(
     name="aiospotify",
     packages=["aiospotify", "aiospotify.objects", "aiospotify.typings"],
     project_urls=PROJECT_URLS,
-    python_requires=">=3.9.0",
+    #python_requires=">=3.9.0",
     url="https://github.com/Axelancerr/aiospotify",
     version=VERSION,
     long_description=LONG_DESCRIPTION,
